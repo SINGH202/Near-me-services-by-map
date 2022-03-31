f2af3c28508674a00193b79d7ecdf8aaf1b86409
ANURAG SINGH <adarshsaurav6613@gmail.com>
2022-03-31 20:20:15 +0530

    removed api key of google maps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import './App.css';
 
 function App() {
-  var axios = require('axios');
-
-var config = {
-  method: 'get',
-  url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyDRRzThdBeKl6L4NKS0DUv94bQzOuixooc',
-  headers: { }
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+  
   return (
     <div className="App">
       <p>map elements</p>
